def78e8bf4848b7ca505f99fe6ff791c12703c53
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-05 11:27:15 +0300

    final

--- data capture.sql
+++ data capture.sql
@@ -14,3 +14,27 @@ customer_id smallint(5) NOT NULL
 ,KEY idx_fk_city_id (city_id)
 )
 ;
+
+INSERT INTO sales.cust_california(
+customer_id
+,address_id
+,district
+,city_id
+,city
+)
+SELECT cus.customer_id
+,adr.address_id
+,adr.district
+,adr.city_id
+,cty.city
+
+FROM sakila.customer cus
+
+JOIN sakila.address adr
+ON cus.address_id = adr.address_id
+
+JOIN sakila.city cty
+ON adr.city_id = cty.city_id
+
+WHERE adr.district = 'California'
+;
